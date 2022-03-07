097f921750f3c5f1e7a7f493375f7734c276a7c2
Mateusz Łątka <mateusz.latka9@gmail.com>
2022-03-07 18:32:20 +0100

    add test environment

--- controllers/testing.js
+++ controllers/testing.js
@@ -1,13 +1,36 @@
 const User = require('../models/User')
 const Comment = require('../models/Comment')
 
+const initialUser = { 
+    username: 'admin', 
+    image: 'https://i.pravatar.cc/150?img=5', 
+    password: 'password123'
+}
+
+const initialComments = [
+    {
+        content: 'Example comment 11',
+        score: 10,
+        parent: null,
+    },
+    {
+        content: 'Example comment 22',
+        score: 20,
+        parent: null,
+    },
+]
+
 const clearAll = async (req, res) =>{
-    await User.deleteMany({});
-    await Comment.deleteMany({});
+    await Comment.deleteMany();
+    await User.deleteMany();
+    const user = await User.create(initialUser)
+    for(let comment of initialComments) {
+        comment.user = user._id;
+        await Comment.create(comment);
+    }
 
     res.status(204).end();
 }
-
 
 module.exports = {clearAll};
 
